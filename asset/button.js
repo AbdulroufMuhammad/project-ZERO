8298b6fc45612cbb69fb5449d633a6cf8fe05188
HITMAN <abdulraufmuhammad28@gmail.com>
2024-06-27 20:57:56 +0200

    Initial commit

---
+++ asset/button.js
@@ -0,0 +1,11 @@
+// ------------------profile--------------------
+const buttonsComponent = document.querySelector('.buttons');
+const buttonsToggle = document.querySelector('.buttons__toggle');
+
+buttonsToggle.addEventListener('click', toggleButtons);
+
+function toggleButtons() {
+	buttonsToggle.classList.toggle('buttons__toggle--active');
+	buttonsComponent.classList.toggle('buttons--active');
+	document.activeElement.blur();
+}
